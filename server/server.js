b4309b545a5e5e020fb995d154ef40b776ddfdde
Kevin Tripp <ktripp1@gmail.com>
2019-01-29 07:09:48 -0500

    add get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongodb  = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,20 @@ app.get('/todos',(req, res) =>{
   }, (e) =>{
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+  var valid = mongodb.ObjectID.isValid(id)
+  if(!valid){
+    res.status(404).send({error:"Id is not valid"})
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      res.status(404).send({error:"No matching records for that id"});
+    }
+    res.status(200).send(todo);
+  }).catch((e) => res.status(400).send({e}));
 });
 
 app.listen(3000, () => {
