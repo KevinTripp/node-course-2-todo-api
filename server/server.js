84afee21507a7d6a61878b9ca6831722762a015f
Kevin Tripp <ktripp1@gmail.com>
2019-02-05 07:07:26 -0500

    Added delete call

--- server/server.js
+++ server/server.js
@@ -46,6 +46,20 @@ app.get('/todos/:id', (req, res) =>{
   }).catch((e) => res.status(400).send({e}));
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!mongodb.ObjectID.isValid(id)){
+    res.status(404).send({error:"Id is not valid"})
+  }
+
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send({error:"No matching records for that id"});
+    }
+    res.status(200).send(todo);
+  }).catch((e) => res.status(400).send({e}));
+});
+
 app.listen(port, () => {
   console.log(`Started server on port ${port}`);
 });
